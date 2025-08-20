4d427026debe154843ee96bb5dae008ec56b5eb3
Sebastian Aguiar Gómez <87918288+AGUIAR-SEBAS@users.noreply.github.com>
2025-08-20 13:00:23 -0500

    Create-portfolio

---
+++ src/components/about/About.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./About.css";
+import Image from "../../assets/avatar-2.svg";
+import Resume from "../../assets/resume.pdf";
+import AboutBox from "./AboutBox";
+
+const About = () => {
+  const downloadResume = async () => {
+    window.open(Resume, "_blank");
+  };
+
+  return (
+    <section className="about container section" id="about">
+      <h2 className="section__title">Quien Soy </h2>
+
+      <div className="about__container grid">
+        <img src={Image} alt="" className="about__img" />
+
+        <div className="about__data grid">
+          <div className="about__info">
+          
+            <p className="about__description">
+             Hola, soy Sebastian Aguiar. Como Ingeniero de Software, transformo desafíos de negocio en soluciones tecnológicas eficientes. Mi experiencia abarca desde el liderazgo técnico y la ingeniería de datos  hasta el desarrollo full-stack. Me motiva crear sistemas que no solo funcionan, sino que optimizan y escalan operaciones. <br /><br />
+             Aquí hay algunas tecnologías con las que he estado trabajando recientemente:
+            </p>
+            <ul className="about__list">
+              <li><b>Frontend:</b> React.js, Next.js, Angular, TypeScript </li>
+              <li><b>Backend:</b> Node.js, Python (Django), C# (.NET), PHP</li>
+              <li><b>Cloud & DevOps:</b> AWS, GCP, Docker, CI/CD con GitHub Actions</li>
+              <li><b>Bases de Datos:</b> PostgreSQL, SQL Server, Oracle, MySql</li>
+              <li><b>Data & Automatización:</b> Power BI, Looker Studio, Make.com, Power Automate</li>
+              <li><b>Otros:</b> WordPress, Git, Office 365, Power Apps, Power Query, Microsoft 365 Admin </li>
+            </ul>
+
+            <button className="btn" onClick={downloadResume}>
+              Descargar HV
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <AboutBox />
+    </section>
+  );
+};
+
+export default About;
